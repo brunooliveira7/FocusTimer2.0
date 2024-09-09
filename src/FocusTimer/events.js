6f766437e132fc0d56e62c6881aa6a17c45c4be6
Bruno Oliveira <brunoroliveira7@gmail.com>
2024-09-09 18:04:20 -0300

    Connecting the first actions

--- src/FocusTimer/events.js
+++ src/FocusTimer/events.js
@@ -3,7 +3,7 @@ import { controls } from "./elements.js";
 export function registerControls() {
   controls.forEach((control) => {
     control.addEventListener("click", (event) => {
-      const action = event.target.dataset.action;
+      const action = event.target.dataset.action; //capturando o btn no html no click
       if (action == undefined) {
         return;
       }
