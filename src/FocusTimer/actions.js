cd67f45fd0d953d7a424679164a984568f9d9949
Bruno Oliveira <brunoroliveira7@gmail.com>
2024-09-09 19:14:35 -0300

    Connecting actions

---
+++ src/FocusTimer/actions.js
@@ -0,0 +1,16 @@
+//serve para o play e pause
+export function toggleRunning() {
+  console.log("toggleRunning Function");
+}
+
+export function reset() {
+  console.log("reset Function");
+}
+
+export function increaseMinutes() {
+  console.log("increaseMinutes Function");
+}
+
+export function decreaseMinutes() {
+  console.log("decreaseMinutes Function");
+}
--- src/FocusTimer/events.js
+++ src/FocusTimer/events.js
@@ -1,13 +1,16 @@
 import { controls } from "./elements.js";
+import * as actions from "./actions.js";
 
+//forEach - para cada botão, se clicar fora return
 export function registerControls() {
   controls.forEach((control) => {
     control.addEventListener("click", (event) => {
-      const action = event.target.dataset.action; //capturando o btn no html no click
-      if (action == undefined) {
+      const action = event.target.dataset.action; //capturando o data-action no html on click individualmente
+      //se o tipo da captura, onclick, não for action (html) return
+      if (typeof actions[action] != "function") {
         return;
       }
-      console.log(action);
+      actions[action]();
     });
   });
 }
